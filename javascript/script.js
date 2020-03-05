ccc967dc10706e20725828d4081840233936f961
Sabaku <pgratkowski87@gmail.com>
2020-03-05 15:35:41 +0100

    Updates

--- javascript/script.js
+++ javascript/script.js
@@ -5,12 +5,81 @@ const menu = document.querySelector(".menu");
 const menuNav = document.querySelector(".menu-nav");
 const menuBranding = document.querySelector(".menu-branding");
 const navItems = document.querySelectorAll(".nav-item");
+const square = document.querySelector("#square");
 
 // Set initial state of menu
 
 let showMenu = false;
 
+class SquareModel {
+  constructor() {
+    this.whichSide = "top";
+  }
+  update(side) {
+    this.whichSide = side;
+  }
+}
+
+class SquareController {
+  constructor(model) {
+    this.model = model;
+  }
+  handleInput(key) {
+    this.key = key;
+    switch (this.key) {
+      case "w":
+        this.model.update("top");
+        console.log(this.key);
+        break;
+      case "s":
+        this.model.update("bottom");
+        console.log(this.key);
+        break;
+      case "d":
+        this.model.update("right");
+        console.log(this.key);
+        break;
+      case "a":
+        this.model.update("left");
+        console.log(this.key);
+        break;
+    }
+  }
+}
+
+class SquareView {
+  constructor(model, controller) {
+    this.model = model;
+    this.controller = controller;
+  }
+  draw() {
+    switch (this.model.whichSide) {
+      case "top":
+        square.style.borderTop = "5px solid red";
+        break;
+      case "bottom":
+        square.style.borderBottom = "5px solid red";
+        break;
+      case "right":
+        square.style.borderRight = "5px solid red";
+        break;
+      case "left":
+        square.style.borderLeft = "5px solid red";
+        break;
+    }
+  }
+}
+
+let squareModel = new SquareModel();
+let squareController = new SquareController(squareModel);
+let squareView = new SquareView(squareModel, squareController);
+console.log(squareModel.whichSide);
+document.addEventListener("keydown", getKey);
 menuBtn.addEventListener("click", toggleMenu);
+
+function getKey(e) {
+  squareController.handleInput(e.key);
+}
 
 function toggleMenu() {
   if (!showMenu) {
